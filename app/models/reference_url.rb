f0d0d986545a935587b6a1cd6d560846c63802a9
Chinmay <chinmay@amurtech.com>
2017-09-11 16:14:24 +0530

    fake data. reference url model

--- app/models/reference_url.rb
+++ app/models/reference_url.rb
@@ -1,4 +1,17 @@
 class ReferenceUrl < ApplicationRecord
 
+  after_initialize :set_defaults, unless: :persisted?
+
   validates :mini_url, :associated_url, :in_use, presence: true
+  validates :mini_url, uniqueness: true
+  validates :associated_url, :format => URI::regexp(%w(http https))
+
+  def set_defaults
+    self.in_use ||= true
+  end
+
+  def generate_random_mini_url
+    (0...16).map{97.+(rand(25)).chr.downcase}.join
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+200.times do
+  reference_url = ReferenceUrl.new(associated_url: Faker::Internet.url)
+  reference_url.mini_url = reference_url.generate_random_mini_url
+  puts "=> Created url: #{reference_url.associated_url}. == mini url code is, #{reference_url.mini_url}"
+  reference_url.save
+end
